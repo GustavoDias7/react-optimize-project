f3f560b14deec874794b6f8fef31647507f81a64
Gustavo <gustavo7dias@gmail.com>
2022-05-30 20:34:44 -0300

    Update GA/GO configs

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import ReactGA from "react-ga";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { root, testB } from "./utils/links";
+ReactGA.initialize("G-YQM0DZ78VN");
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize("G-YQM0DZ78VN");
-  }, []);
   return (
     <Routes>
       <Route path={root} element={<Home />} />
