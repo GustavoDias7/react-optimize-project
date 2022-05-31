6d30a822882c411bd4dce466a7056b24c7c4b81b
Gustavo <gustavo7dias@gmail.com>
2022-05-31 15:19:00 -0300

    Update GA/GO configs 10

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,16 +9,28 @@ const Home = () => {
   const location = useLocation();
 
   useEffect(() => {
-    console.log({ location: location });
     if (!window.GA_INITIALIZED) {
       ReactGA.initialize("UA-230277864-1");
       window.GA_INITIALIZED = true;
     }
     ReactGA.set({ page: location.pathname });
     ReactGA.pageview(location.pathname);
+    (function (a, s, y, n, c, h, i, d, e) {
+      s.className += " " + y;
+      h.start = 1 * new Date();
+      h.end = i = function () {
+        s.className = s.className.replace(RegExp(" ?" + y), "");
+      };
+      (a[n] = a[n] || []).hide = h;
+      setTimeout(function () {
+        i();
+        h.end = null;
+      }, c);
+      h.timeout = c;
+    })(window, document.documentElement, "async-hide", "dataLayer", 4000, {
+      CONTAINER_ID: true,
+    });
   }, [location]);
-
-  // console.log({ variant });
 
   return (
     <div className={style.wrapper}>
