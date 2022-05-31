bf27dafb1ea003280f4ec92a3cefec3b52519db2
Gustavo <gustavo7dias@gmail.com>
2022-05-31 15:50:24 -0300

    Update GA/GO configs 14

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,23 +15,6 @@ const Home = () => {
     }
     ReactGA.set({ page: location.pathname });
     ReactGA.pageview(location.pathname);
-    // <script>
-    (function (a, s, y, n, c, h, i, d, e) {
-      s.className += " " + y;
-      h.start = 1 * new Date();
-      h.end = i = function () {
-        s.className = s.className.replace(RegExp(" ?" + y), "");
-      };
-      (a[n] = a[n] || []).hide = h;
-      setTimeout(function () {
-        i();
-        h.end = null;
-      }, c);
-      h.timeout = c;
-    })(window, document.documentElement, "async-hide", "dataLayer", 4000, {
-      "OPT-P3JFSPD": true,
-    });
-    // </script>
   }, [location]);
 
   return (
