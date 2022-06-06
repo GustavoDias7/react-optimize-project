05baf1a572ab5fc9303bd797cbb0f7be870d6062
Gustavo <gustavo7dias@gmail.com>
2022-06-06 17:45:17 -0300

    Update metrics configs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Metrics from "./google/Metrics";
 import Home from "./pages/Home";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
 import { page1, page2, root, testB } from "./utils/links";
-import { init } from "./utils/metrics";
 
 function App() {
-  useEffect(() => {
-    init();
-  }, []);
-
   return (
     <Routes>
-      <Route path={testB} element={<Metrics />} />
       <Route path={root} element={<Home />} />
       <Route path={testB} element={<Home />} />
       <Route path={page1} element={<Page1 />} />
