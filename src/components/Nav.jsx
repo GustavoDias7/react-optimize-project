72cfda525fbe1b1e27674168a7e7bf82eeb26e3a
Gustavo <gustavo7dias@gmail.com>
2022-06-08 12:33:11 -0300

    Update GA/GTM configs 11

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,22 @@ import { page1, page2, root, testB } from "../utils/links";
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <Link to={root}>Home</Link>
-      <Link to={testB}>Test B</Link>
-      <Link to={page1}>Page1</Link>
-      <Link to={page2}>Page2</Link>
-    </nav>
+    <>
+      <nav className="nav">
+        <p>Component Link</p>
+        <Link to={root}>Home</Link>
+        <Link to={testB}>Test B</Link>
+        <Link to={page1}>Page1</Link>
+        <Link to={page2}>Page2</Link>
+      </nav>
+      <nav className="nav">
+        <p>Tag a</p>
+        <a href={root}>Home</a>
+        <a href={testB}>Test B</a>
+        <a href={page1}>Page1</a>
+        <a href={page2}>Page2</a>
+      </nav>
+    </>
   );
 };
 
