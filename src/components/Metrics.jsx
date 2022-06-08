f3f668b7c42a060d7513182b17db2e6356249442
Gustavo <gustavo7dias@gmail.com>
2022-06-08 12:27:12 -0300

    Update GA/GTM configs 10

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import TagManager from "react-gtm-module";
-import { tagManagerArgs } from "../utils/metrics";
+import { init } from "../utils/metrics";
 
 const Metrics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
+    init();
   }, [location]);
 
   return <div>Metrics</div>;
